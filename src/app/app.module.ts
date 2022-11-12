b3716f41a9410aa7bae0de40b1559efcd9df3d45
Utkarsh Padia <utkarshpadia@Utkarshs-MacBook-Pro.local>
2022-11-13 02:50:53 +0530

    initialize portfolio

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
